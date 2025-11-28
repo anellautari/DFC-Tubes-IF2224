3791979c3ecca48f49219bd11284d949797823a4
anellautari <19623229@std.stei.itb.ac.id>
2025-11-28 16:44:13 +0700

    feat: update app flow and output for semantic analyzing

--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 import sys
 from src.lexer.lexer import Lexer
-from src.common.utils import load_dfa_rules, read_source_code
+from src.common.utils import load_dfa_rules, read_source_code, print_symbol_tables, print_ast_tree
 from src.parser.parser import Parser
+from src.semantic.ast_builder import ASTBuilder
+from src.semantic.semantic_analyzer import SemanticAnalyzer
 
 def app():
     if len(sys.argv) != 2:
@@ -10,19 +12,27 @@ def app():
 
     source_path = sys.argv[1]
 
-    if source_path.endswith(".pas"):
-        source = read_source_code(source_path)
-        dfa_rules = load_dfa_rules()
-        
-        lexer = Lexer(source, dfa_rules)
-        tokens = lexer.tokenize()
-        
-        for token in tokens:
-           print(token)
-        
-        parser = Parser(tokens)
-        parser.parse_program()
-
-    else:
+    if not source_path.endswith(".pas"):
         print("Error: Input file harus berekstensi .pas")
         sys.exit(1)
+
+    source = read_source_code(source_path)
+    dfa_rules = load_dfa_rules()
+    lexer = Lexer(source, dfa_rules)
+    tokens = lexer.tokenize()
+
+    parser = Parser(tokens)
+    parse_tree_root = parser.parse_program()
+
+    builder = ASTBuilder()
+    ast_root = builder.build(parse_tree_root)
+
+    analyzer = SemanticAnalyzer()
+    analyzer.visit(ast_root)
+
+    print("\n===== SYMBOL TABLES =====")
+    print_symbol_tables(analyzer.symtab)
+
+    print("\n===== DECORATED AST =====")
+    print_ast_tree(ast_root)
+
